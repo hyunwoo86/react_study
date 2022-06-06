7b49700f614361fedd822980c2dc2c5db7295a51
HyunWoo Jung <junghw86@naver.com>
2022-06-07 06:34:07 +0900

    React Hooks에 취한다 - useRef 완벽 정리 1# 변수 관리 | 리액트 훅스 시리즈
    var 로 하면 랜더링 되면 값이 초기화가된다,
    하지만 useRef를 가지고 있으므로 데이터를 유지 할 수 있다.

--- react_study_room/src/App.js
+++ react_study_room/src/App.js
@@ -2,23 +2,30 @@ import "./App.css";
 import { useState, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [render, setRenderer] = useState(0);
   const countRef = useRef(0);
+  let countVar = 0;
+
   console.log("rendering...");
   const handleIncreaseCount = () => {
-    setCount(count + 1);
+    countVar += 1;
+    console.log("Var : " + countVar);
   };
   const handleRefCount = () => {
     countRef.current += 1;
     console.log("ref : " + countRef.current);
   };
 
+  const handleRender = () => {
+    setRenderer(1);
+  };
   return (
     <div className="starcoding">
-      <p>State: {count}</p>
+      <p>Var: {countVar}</p>
       <p>Ref: {countRef.current}</p>
-      <button onClick={handleIncreaseCount}>state 출력</button>
+      <button onClick={handleIncreaseCount}>Var 출력</button>
       <button onClick={handleRefCount}>Ref 출력</button>
+      <button onClick={handleRender}>Rendering</button>
     </div>
   );
 }
