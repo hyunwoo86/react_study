45e11cc5ad5d5043cf59f1fc2e75825fe4480da1
HyunWoo Jung <junghw86@naver.com>
2022-06-07 00:24:08 +0900

    Revert "React Hooks에 취한다 - useEffect 깔끔하게 마스터하기 | 리액트 훅스 시리즈"
    This reverts commit c35678ce591a5dd1e4c1f2d66c2cd6f513d1730f.

--- react_study_room/src/App.js
+++ react_study_room/src/App.js
@@ -1,47 +1,23 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Timer from "./component/Timer";
-
-function Out_function() {
-  // console.log("outFunction");
-}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  // useEffect(Out_function);
-
-  // const [count, setCount] = useState(1);
-  // const [name, setName] = useState("");
-
-  // // useEffect(() => {
-  // //   console.log("count rendering");
-  // // }, [count]);
-
-  // useEffect(() => {
-  //   console.log("first rendering");
-  // }, []);
-
-  // const handleCountUpdate = () => {
-  //   setCount(count + 1);
-  // };
-
-  // const handleInputName = (e) => {
-  //   console.log();
-  //   setName(e.target.value);
-  // };
-  const [showTimer, setShowTimer] = useState(false);
-
-  const handleShowTimer = () => {
-    setShowTimer(!showTimer);
-  };
   return (
-    <div className="starcoding">
-      {showTimer && <Timer></Timer>}
-      <button onClick={handleShowTimer}>Toggle Timer</button>
-      {/* <button onClick={handleCountUpdate}>Update</button>
-      <span>count: {count}< /span>
-      <br></br>
-      <input type="text" onChange={handleInputName}></input>
-      name: {name} */}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
