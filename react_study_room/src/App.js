a6bdffd889a70079d257870c8d40073a6856610c
HyunWoo Jung <junghw86@naver.com>
2022-06-11 21:42:43 +0900

    import styles from "./CheckBox.module.css"; import classNames from "classnames/bind";
    const cx = classNames.bind(styles);
    
    <div className={styles.checkbox}> 을
    
    <div className={cx("checkbox")}> 로 변경하여 더 단순하게 작성 할 수가 있다.

--- react_study_room/src/App.js
+++ react_study_room/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import Box from "./components/Box";
+import CheckBox from "./components/CheckBox";
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
   return (
     <div>
-      <Box></Box>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? "true" : "false"}
+      </p>
     </div>
   );
 }
