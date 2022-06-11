0ef494b95af32e195b8b488ff9f871776d18bb7e
HyunWoo Jung <junghw86@naver.com>
2022-06-11 21:21:39 +0900

    Box.module.css
    css 중간에 module를 넣어 class 명을 중복 되는 것을 막아 준다,

--- react_study_room/src/App.js
+++ react_study_room/src/App.js
@@ -1,63 +1,11 @@
 import React from "react";
 import "./App.scss";
-import Button from "./components/Button";
+import Box from "./components/Box";
 
 function App() {
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button
-          size="large"
-          onClick={() => {
-            console.log("클릭 되었다.");
-          }}
-        >
-          BUTTON
-        </Button>
-        <Button>BUTTON</Button>
-        <Button size="small">BUTTON</Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="gray">
-          BUTTON
-        </Button>
-        <Button color="gray">BUTTON</Button>
-        <Button size="small" color="gray">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          BUTTON
-        </Button>
-        <Button color="pink">BUTTON</Button>
-        <Button size="small" color="pink">
-          BUTTON
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="blue" outline>
-          BUTTON
-        </Button>
-        <Button color="gray" outline>
-          BUTTON
-        </Button>
-        <Button size="small" color="pink" outline>
-          BUTTON
-        </Button>
-      </div>
-
-      <div className="buttons">
-        <Button size="large" fullWidth>
-          BUTTON
-        </Button>
-        <Button size="large" fullWidth color="gray">
-          BUTTON
-        </Button>
-        <Button size="large" fullWidth color="pink">
-          BUTTON
-        </Button>
-      </div>
+    <div>
+      <Box></Box>
     </div>
   );
 }
