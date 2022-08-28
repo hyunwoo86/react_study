0b31f85459a09e789bb5f3b1e432ad76a69c2d1f
HyunWoo Jung <junghw86@naver.com>
2022-08-28 13:08:20 +0900

    React.Fragment 예시
    불필요한 div 표시를 줄일 수 있다.

--- react_study_room/src/App.js
+++ react_study_room/src/App.js
@@ -1,28 +1,10 @@
-import { useCallback, useState } from "react";
-import Child from "./component/Child";
+import React from "react";
+import Components from "./Components";
 
 function App() {
-  const [parentAge, setParentAge] = useState(0);
-
-  const incrementParentAge = () => {
-    setParentAge(parentAge + 1);
-  };
-
-  const telMe = useCallback(() => {
-    console.log("사랑한다 ;)");
-  }, []); // useCallback을 통하여 해당 함수에 대해서 자녀 컴포넌트 재 랜더링 되는것을 막아준다.
-
-  //   const telMe = () => {
-  //     console.log("사랑한다 ;)");
-  //   };
-
-  console.log("부모 컴퍼넌트가 랜더링 되었습니다.");
   return (
-    <div style={{ border: "2px solid navy", padding: "10px" }}>
-      <h1>😍부모</h1>
-      <p>age: {parentAge}</p>
-      <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <Child name={"홍길동"} telMe={telMe}></Child>
+    <div className="App">
+      <Components></Components>
     </div>
   );
 }
---
+++ react_study_room/src/Components.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Components = () => {
+  return (
+    <React.Fragment>
+      <h1>안녕하세요 리액트</h1>
+      <p>반가워요</p>
+    </React.Fragment>
+  );
+};
+export default Components;
